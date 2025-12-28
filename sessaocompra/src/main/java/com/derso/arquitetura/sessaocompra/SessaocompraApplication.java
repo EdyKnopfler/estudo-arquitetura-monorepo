659f9cbb45be62b618d94d07ea6664b24d9960ae
Éderson Cássio <edersoncassio1983@gmail.com>
2025-12-28 15:46:25 -0300

    Serviço de sessões de compra criado no Spring Initializr

---
+++ sessaocompra/src/main/java/com/derso/arquitetura/sessaocompra/SessaocompraApplication.java
@@ -0,0 +1,13 @@
+package com.derso.arquitetura.sessaocompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessaocompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessaocompraApplication.class, args);
+	}
+
+}
